e24da50692ec0fb82c4b6dddc66d1fb070e6d6d6
Yen Mai <yenmai477@gmail.com>
2020-06-13 07:47:15 +0700

    Update api fix svm and add pre train results

--- App.py
+++ App.py
@@ -10,7 +10,8 @@ import numpy as np
 from flask import Flask, request, jsonify
 import joblib
 import pickle
-from PredictHelper import LSTM_predict, predict
+from PredictHelper import LSTM_predict, predict, pre_processing, vectorize_lstm, vectorize_clasifer
+import keras.backend.tensorflow_backend as tb
 
 app = Flask(__name__)
 
@@ -29,6 +30,8 @@ def main():
 
 @app.route('/api', methods=['POST'])
 def predictApi():
+
+    tb._SYMBOLIC_SCOPE.value = True
     # Get the data from the POST request.
     data = request.get_json()
     print(data)
@@ -37,15 +40,104 @@ def predictApi():
     # Make prediction using model loaded from disk as per the data.
 
     output_LSTM = LSTM_predict(comment, model_LSTM, tokenizer_LSTM)
-
+    output_SVM = predict(comment, SVM_model, tfidf_vec)
     output_NB = predict(comment, NB_model, tfidf_vec)
 
     # return result
-    return jsonify({"LSTM": output_LSTM, "NB": output_NB, })
+    return jsonify({"LSTM": output_LSTM, "NB": output_NB, "SVM": output_SVM})
+
+
+@app.route('/api/lstm', methods=['POST'])
+def pridictLSTM():
+
+    tb._SYMBOLIC_SCOPE.value = True
+
+    # Get the data from the POST request.
+    data = request.get_json()
+    print(data)
+    comment = data["comment"]
+
+    output = LSTM_predict(comment, model_LSTM, tokenizer_LSTM)
+
+    standardize, tokens, w_lenmatizer = pre_processing(comment)
+
+    convert_vector = vectorize_lstm(w_lenmatizer, tokenizer_LSTM)
+
+    string_vector = np.array_str(convert_vector)
+
+    data = {
+        'Result': output,
+        'standardize': standardize,
+        'tokens': tokens,
+        'lenmatizer': str(w_lenmatizer),
+        'vector': string_vector
+    }
+
+    return jsonify(data)
+
+
+@app.route('/api/svm', methods=['POST'])
+def pridictModelSVM():
+    tb._SYMBOLIC_SCOPE.value = True
+
+    data = request.get_json()
+    print(data)
+    comment = data["comment"]
+
+    output = predict(comment, SVM_model, tfidf_vec)
+
+    standardize, tokens, w_lenmatizer = pre_processing(comment)
+
+    convert_vector = vectorize_clasifer(w_lenmatizer, tfidf_vec)
+
+    string_vector = str(convert_vector)
+
+    if output is None:
+        output = "NULL"
+
+    data = {
+        'Result': output,
+        'standardize': standardize,
+        'tokens': tokens,
+        'lenmatizer': str(w_lenmatizer),
+        'vector': string_vector
+    }
+
+    return jsonify(data)
+
+
+@app.route('/api/nb', methods=['POST'])
+def pridictModelNP():
+    tb._SYMBOLIC_SCOPE.value = True
+
+    data = request.get_json()
+    print(data)
+    comment = data["comment"]
+
+    output = predict(comment, NB_model, tfidf_vec)
+
+    standardize, tokens, w_lenmatizer = pre_processing(comment)
+
+    convert_vector = vectorize_clasifer(w_lenmatizer, tfidf_vec)
+
+    string_vector = str(convert_vector)
+
+    if output is None:
+        output = "NULL"
+
+    data = {
+        'Result': output,
+        'standardize': standardize,
+        'tokens': tokens,
+        'lenmatizer': str(w_lenmatizer),
+        'vector': string_vector
+    }
+
+    return jsonify(data)
 
 
 if __name__ == '__main__':
     try:
-        app.run(port=5000, threaded=False)
+        app.run(port=5000, threaded=False, debug=True)
     except:
         print("Server is exited unexpectedly. Please contact server admin.")
--- PredictHelper.py
+++ PredictHelper.py
@@ -2,13 +2,78 @@ from flask import Flask, jsonify, request
 import numpy as np
 from string import digits, punctuation
 import re
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import wordnet
+import string
 from keras.preprocessing.sequence import pad_sequences
+from nltk.corpus import wordnet, stopwords
+from nltk.stem import WordNetLemmatizer
 import nltk
 
 
-# Lemmatization
+def standardize_data(text):
+    # Replace email addresses with 'email'
+    re_email = re.compile('[\w\.-]+@[\w\.-]+(\.[\w]+)+')
+    text = re.sub(re_email, 'email', text)
+
+    # Replace URLs with 'webaddress'
+    re_url = re.compile(
+        '(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?')
+    text = re.sub(re_url, 'webaddress', text)
+
+    # Replace money symbols with 'moneysymb'
+    re_moneysb = re.compile('\$')
+    text = re.sub(re_moneysb, 'moneysb', text)
+
+    # Remove ufeff
+    re_moneysb = re.compile('\ufeff|\\ufeff')
+    text = re.sub(re_moneysb, ' ', text)
+
+    # Replace 10 digit phone numbers (formats include paranthesis, spaces, no spaces, dashes) with 'phonenumber'
+    re_phonenb = re.compile(
+        '(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
+    text = re.sub(re_phonenb, 'phonenb', text)
+
+    # Replace numbers with 'numbr'
+    re_number = re.compile('\d+(\.\d+)?')
+    text = re.sub(re_number, ' numbr ', text)
+
+    # Remove puntuation
+    text = text.translate(str.maketrans('', '', punctuation))
+
+    # Replace whitespace between terms with a single space
+    re_space = re.compile('\s+')
+    text = re.sub(re_space, ' ', text)
+
+    # Remove leading and trailing whitespace
+    re_space = re.compile('^\s+|\s+?$')
+    text = re.sub(re_space, ' ', text)
+
+    return text
+
+
+def remove_stopwords(text):
+    stop_words = set(stopwords.words('english'))
+    token = [term for term in text.split() if term not in stop_words]
+    return token
+
+
+def word_lenmatizer(token):
+    # Init Lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    hl_lemmatized = []
+    lemm = [lemmatizer.lemmatize(w, get_wordnet_pos(w)) for w in token]
+    hl_lemmatized.append(lemm)
+    return hl_lemmatized
+
+
+def vectorize_lstm(w_lenmatizer, tokenizer):
+    sequences = tokenizer.texts_to_sequences(w_lenmatizer)
+    X = pad_sequences(sequences, maxlen=103)
+    return X
+
+
+def vectorize_clasifer(w_lenmatizer, tfidf_vector):
+    w_lenmatizer = [" ".join(x) for x in w_lenmatizer]
+    return tfidf_vector.transform(w_lenmatizer)
 
 
 def get_wordnet_pos(word):
@@ -22,46 +87,42 @@ def get_wordnet_pos(word):
     return tag_dict.get(tag, wordnet.NOUN)
 
 
-def word_lenmatizer(text):
-    cleanr = re.compile('<.*?>')
-    text = re.sub(cleanr, ' ', text)
-    clean = text.translate(str.maketrans('', '', punctuation))
-    clean = clean.translate(str.maketrans('', '', digits))
-    text_tokenizer = clean.split()
-    # Init Lemmatizer
-    lemmatizer = WordNetLemmatizer()
-    hl_lemmatized = []
-    lemm = [lemmatizer.lemmatize(w, get_wordnet_pos(w))
-            for w in text_tokenizer]
-    hl_lemmatized.append(lemm)
-    return hl_lemmatized
+def pre_processing(text):
+    text = text.lower()
+    standardize = standardize_data(text)
+
+    tokens = remove_stopwords(standardize)
+
+    w_lenmatizer = word_lenmatizer(tokens)
+
+    return str(standardize), str(tokens), w_lenmatizer
 
 
 def LSTM_predict(text, model, tokenizer):
-    w_lenmatizer = word_lenmatizer(text)
-    max_token = 14
+    text = standardize_data(text)
+    token = remove_stopwords(text)
+    w_lenmatizer = word_lenmatizer(token)
+    max_token = 103
     sequences = tokenizer.texts_to_sequences(w_lenmatizer)
     X = pad_sequences(sequences, maxlen=max_token)
-    print(model.predict(X))
     if np.around(model.predict(X)[0]) == 1:
-        print(text)
         print("=============> SPAM\n")
-        return "SPAM COMMENT"
+        return "SPAM"
     else:
-        print(text)
         print("=============> HAM\n")
-    return "HAM COMMENT"
+        return "HAM"
 
 
 def predict(text, model, tfidf_vector):
-    w_lenmatizer = word_lenmatizer(text)
+    print(text)
+    text = standardize_data(text)
+    token = remove_stopwords(text)
+    w_lenmatizer = word_lenmatizer(token)
     w_lenmatizer = [" ".join(x) for x in w_lenmatizer]
     X_Tfidf = tfidf_vector.transform(w_lenmatizer)
     if model.predict(X_Tfidf)[0] == 1:
-        print(text)
         print("=============> SPAM\n")
-        return "SPAM COMMENT"
+        return "SPAM"
     else:
-        print(text)
         print("=============> HAM\n")
-        return "HAM COMMENT"
+        return "HAM"
